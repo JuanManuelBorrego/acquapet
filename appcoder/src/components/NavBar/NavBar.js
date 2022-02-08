4c5791241f72d7be8cff2c46cb6210d4987448b3
Juan Manuel Borrego <juanmaborrego2@gmail.com>
2022-02-07 23:27:09 -0300

    NavBar creado

---
+++ appcoder/src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbara">
+            <ul>
+                <li className="li">
+                    <a className="a" href="https://www.google.com/">ACQUA</a>
+                </li>
+            </ul>
+            <ul>
+            <li className="li">
+                    <a className="a" href="https://www.ole.com.ar/">PET</a>
+                </li>
+            </ul>
+            <h2 className="h2">ACQUAPET</h2>
+        </nav>
+    )
+}
+
+export default NavBar
